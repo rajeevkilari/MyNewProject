54fa28adb70c5b07663964823aaa90eb776bc5dc
Bhushan <nbhushan@teksystems.com>
2023-02-06 12:10:42 +0530

    fixed Issue With CodeCommit

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -2,19 +2,20 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 	@Autowired
-	private StudentInterface interface1;
+	private StudentInterface studentInterface;
 
 	@PostMapping("/savedstudents")
-	public StudentEntity saveinfo(StudentEntity entity) {
+	public StudentEntity saveStudentInfo(@RequestBody StudentEntity entity) {
 		StudentEntity studentEntity = new StudentEntity();
 		studentEntity.setName(entity.getName());
 		studentEntity.setSection(entity.getSection());
-		return interface1.save(studentEntity);
-
+		return studentInterface.save(studentEntity);
 	}
 }
