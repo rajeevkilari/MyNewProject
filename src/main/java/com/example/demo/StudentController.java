0db9563429a801867474b62e8808c077bff0f9c6
Bhushan <nbhushan@teksystems.com>
2023-02-07 10:05:31 +0530

    added some more methods in controller

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -1,10 +1,9 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class StudentController {
@@ -17,5 +16,24 @@ public class StudentController {
 		studentEntity.setName(entity.getName());
 		studentEntity.setSection(entity.getSection());
 		return studentInterface.save(studentEntity);
+	}
+
+	@GetMapping("/savedstudents/{studentId}")
+	public Optional<StudentEntity> getStudentById(@PathVariable Integer studentId) {
+		Optional<StudentEntity> studentEntity = studentInterface.findById(studentId);
+		if (!studentEntity.isPresent()) {
+			return null;
+		} else {
+			return studentEntity;
+		}
+	}
+	@DeleteMapping("/deleteStudents/{studentId}")
+	public String deleteStudentByid(@PathVariable Integer studentId) {
+		studentInterface.deleteById(studentId);
+		if (getStudentById(studentId).isPresent()) {
+			return "Data has Not been Deleted in Database";
+		} else {
+			return "Record has been deleted in Database";
+		}
 	}
 }
