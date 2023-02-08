ad70ea7719b3c4fc94f7d3d8534bb6ed18406ef4
CodeTalks <info@codetalksdna.com>
2023-02-08 10:10:17 +0530

    Added Packages

--- src/main/java/com/example/demo/StudentInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface StudentInterface extends JpaRepository<StudentEntity, Integer>{
-
-}
