45f669db7f52354742162f7c1069891318dd1e53
CodeTalks <info@codetalksdna.com>
2023-02-11 23:32:26 +0530

    Removed Unused Code

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -3,15 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.response.Response;
 import com.example.demo.service.StudentService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
 	@Autowired
 	public StudentService studentService;
 
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -3,32 +3,24 @@ package com.example.demo.service;
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.response.Response;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(StudentService.class);
-    
     @Autowired
     public StudentRepository studentRepository;
 
     public Response saveStudentInfo(StudentEntity entity) {
-        LOGGER.info("Student Id is {}",entity.getID());
         return studentRepository.saveStudentsData(entity);
     }
 
     public Response getStudentById(Integer studentId) {
-        LOGGER.info("Student Id is {}",studentId);
         return studentRepository.getStudentById(studentId);
     }
 
     public Response delStudentById(Integer studentId) {
-        LOGGER.info("Student Id is {}",studentId);
         return studentRepository.delStudentById(studentId);
-
     }
 }
